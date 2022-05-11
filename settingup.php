f38fd0fd8f7e38b1d16271c0b7cbe7c5c384515b
tbanda <tbanda@ADD-TIMOTHYKB>
2022-05-11 14:06:43 +0200

    Feat: run sql file

---
+++ settingup.php
@@ -0,0 +1,23 @@
+<?php
+
+$filename = "sqlData/e_tutoring.sql";
+
+
+$templine = '';
+$lines = file($filename);
+foreach ($lines as $line){
+
+	if (substr($line, 0, 2) == '--' || $line == '')
+		continue;
+
+		$templine .= $line;
+
+		if (substr(trim($line), -1, 1) == ';')
+		{
+
+			mysqli_query($link, $templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysqli_error($link) . '<br /><br />');
+
+			$templine = '';
+		}
+}
+header("Location: index.php");
